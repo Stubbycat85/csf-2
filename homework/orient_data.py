77a15fde6faf280e816476f18da70393293bae79
Paul Pham <ppham@local-box.org>
2013-10-30 23:17:14 -0700

    Updated calendar for hw5. Added hw5 and Orient Express files.

---
+++ homework/orient_data.py
@@ -0,0 +1,14 @@
+def read_journey_segments():
+	pass
+
+def read_passenger_statements():
+	pass
+
+def read_passenger_names():
+	pass
+
+def read_menu_selections():
+	pass
+
+def shuffle_integers():
+	pass
